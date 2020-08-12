cfa0264ef1a48fa7d5edb3e8bff8336ad14cbec3
Your Name <you@example.com>
2020-08-12 13:54:29 +0100

    add model and augment-image worker

---
+++ README.md
@@ -0,0 +1,5 @@
+sources:
+
+- https://github.com/ya9do/tfjs-data-generator-sample/blob/master/src/index.ts
+- https://github.com/tensorflow/tfjs-examples/blob/master/iris-fitDataset/data.js
+- https://js.tensorflow.org/api/latest/#data.generator
---
+++ src/augment-image/augment-image-worker.ts
@@ -0,0 +1,36 @@
+import { random, sampleSize } from "lodash";
+
+import jimp from "jimp";
+import { rotate } from "./rotate";
+import { scale } from "./scale";
+import { shiftHeight } from "./shift-height";
+import { shiftWidth } from "./shift-width";
+import workerpool from "workerpool";
+
+const operations = [
+  (image: jimp) => image.flip(true, false),
+  (image: jimp) => shiftWidth(image, 0.15),
+  (image: jimp) => shiftHeight(image, 0.15),
+  (image: jimp) => scale(image, 0.5),
+  (image: jimp) => rotate(image, 45)
+];
+
+const generateAugmentImage = async (path: string, shape: number) => {
+  const image = (await jimp.read(path)).resize(
+    shape,
+    shape
+  );
+  const operationsImageResult = sampleSize(
+    operations,
+    random(0, operations.length)
+  ).reduce((image: jimp, operation: (image: jimp) => jimp) => operation(image), image);
+  
+  // await operationsImageResult.writeAsync(`test_photo/${path}`);
+  // console.log(`test_photo/${path}`)
+  const buffer = await operationsImageResult.getBufferAsync(jimp.MIME_PNG);
+  return buffer;
+};
+
+workerpool.worker({
+  generateAugmentImage: generateAugmentImage,
+});
